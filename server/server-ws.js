09b183511dbb528625067c8957ab611c73bacbc5
Codallo Server <server@codallo.com>
2025-09-25 22:54:39 +0200

    implement websocket server

---
+++ server/server-ws.js
@@ -0,0 +1,47 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+
+function createWsServer(port) {
+  const wss = new WebSocketServer({ port });
+
+  let controllerSocket = null;
+  let displaySocket = null;
+
+  wss.on("connection", (ws, req) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const role = url.searchParams.get("role");
+
+    if (role === "sensor") {
+      controllerSocket = ws;
+      console.log("Sensor connected");
+
+      // Forward controller messages to display (if connected)
+      ws.on("message", (msg) => {
+        console.log(`    Sensor message: ${msg}`);
+        if (displaySocket && displaySocket.readyState === WebSocket.OPEN) {
+          displaySocket.send(msg);
+        }
+      });
+
+      ws.on("close", () => {
+        console.log("controller disconnected");
+        controllerSocket = null;
+      });
+    }
+
+    if (role === "display") {
+      displaySocket = ws;
+      console.log("Display connected");
+
+      ws.on("close", () => {
+        console.log("Display disconnected");
+        displaySocket = null;
+      });
+    }
+  });
+
+  return wss;
+}
+
+
+export default createWsServer;
