9165e91c911a8f1bd2b064f440cb03b921f5fb77
Miroslav Junker <junker@codeversa.com>
2025-09-27 17:43:26 +0200

    render basic 3D model

--- display/nbr-plane/src/App.tsx
+++ display/nbr-plane/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.tsx
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Box() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>NoBrakes 3D</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <mesh rotation={[0.4, 0.2, 0]}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-3">My 3D App</h1>
+      <Canvas style={{ height: '500px', background: '#eee' }}>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box />
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
