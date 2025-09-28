c9fc8ff035031f5bbdbee548431e752b2f0834be
Miroslav Junker <junker@codeversa.com>
2025-09-28 17:39:48 +0200

    rename frontend applications

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,6 @@
 
 PROJECT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-cd "$PROJECT_DIR/display" && npm ci && npm run build
+cd "$PROJECT_DIR/app-display" && npm ci && npm run build
 
 cd "$PROJECT_DIR"
