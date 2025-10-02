8355f2624368d9433992f63c0b0918f783ec5a56
Miroslav Junker <junker@codeversa.com>
2025-10-03 00:48:45 +0200

    set more realistic parachute position

--- app-display/src/physics/state.ts
+++ app-display/src/physics/state.ts
@@ -4,7 +4,7 @@ import type { TGameState } from '../types';
 export const initGameState: TGameState = {
     planePosX: 0,
     planeRotationX: 0,
-    parachutePos: [20, 0, 40]
+    parachutePos: [5, 0, 40]
 }
 
-export const COLLISION_DISTANCE = 5;
+export const COLLISION_DISTANCE = 3;
