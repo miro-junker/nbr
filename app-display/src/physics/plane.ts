440c20118dbe6af4709b22727d93c09c94938002
Miroslav Junker <junker@codeversa.com>
2025-10-04 19:50:30 +0200

    implement vertical plane rotating

--- app-display/src/physics/plane.ts
+++ app-display/src/physics/plane.ts
@@ -1,21 +1,33 @@
 import type { TGameState, TSteering } from '../types';
 
 
-const PLANE_ROTATION_INERTIA = 0.5;
+const PLANE_ROTATION_X_INERTIA = 0.5;
+const PLANE_ROTATION_Y_INERTIA = 0.5
 const PLANE_HORIZONTAL_SPEED = 10;
 const MAX_OFFSET_X = 17.5;
 
 
-export const getPlaneRotation = (gameState: TGameState, steering: TSteering, delta: number) => {
-    const calculated = gameState.planeRotationX + steering.horizontal * (delta * PLANE_ROTATION_INERTIA)
+export const getPlaneRotX = (gameState: TGameState, steering: TSteering, delta: number) => {
+    const calculated = gameState.planeRotationX + steering.horizontal * (delta * PLANE_ROTATION_X_INERTIA)
     if (calculated >= 1) return 1
     if (calculated <= -1) return -1
     return calculated
 }
 
-export const getPlanePositionX = (gameState: TGameState, newPlaneRotationX: number, delta: number) => {
+export const getPlanePosX = (gameState: TGameState, newPlaneRotationX: number, delta: number) => {
     const calculated = gameState.planePosX + newPlaneRotationX * delta * PLANE_HORIZONTAL_SPEED
     if (calculated >= MAX_OFFSET_X) return MAX_OFFSET_X
     if (calculated <= -MAX_OFFSET_X) return -MAX_OFFSET_X
     return calculated
+}
+
+export const getPlaneRotY = (gameState: TGameState, steering: TSteering, delta: number) => {
+    const calculated = gameState.planeRotationY + steering.vertical * (delta * PLANE_ROTATION_Y_INERTIA)
+    if (calculated >= 1) return 1
+    if (calculated <= -1) return -1
+    return calculated
+}
+
+export const getPlanePosY = (gameState: TGameState, steering: TSteering, delta: number) => {
+    // TBD
 }
--- app-display/src/physics/state.ts
+++ app-display/src/physics/state.ts
@@ -3,7 +3,9 @@ import type { TGameState } from '../types';
 
 export const initGameState: TGameState = {
     planePosX: 0,
+    planePosY: 0,
     planeRotationX: 0,
+    planeRotationY: 0,
     parachutePos: [5, 0, 40]
 }
 
