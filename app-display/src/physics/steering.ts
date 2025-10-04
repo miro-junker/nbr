06b017bca53d623599bdc0ba4cb14c957e6c5de7
Miroslav Junker <junker@codeversa.com>
2025-10-04 19:49:37 +0200

    store raw sensor data

--- app-display/src/physics/steering.ts
+++ app-display/src/physics/steering.ts
@@ -1,5 +1,9 @@
 import type { TSteering } from "../types";
 
 export const initialSteering: TSteering = {
-    horizontal: 0
+    horizontal: 0,
+    vertical: 0,
+    a: 0,
+    b: 0,
+    c: 0,
 }
--- app-display/src/types/steering.ts
+++ app-display/src/types/steering.ts
@@ -1,5 +1,10 @@
 export type TSteering = {
     horizontal: number; // -1 (left) to 1 (right)
     vertical: number;   // -1 (down) to 1 (up)
+   
+    // raw sensor data
+    a: number;          
+    b: number;
+    c: number;
 }
 
