ef7ff924de75b27f21ce5b62a1cf9a8b184cd44b
Miroslav Junker <junker@codeversa.com>
2025-10-04 23:41:09 +0200

    prepare DB structure

---
+++ db/init-db.js
@@ -0,0 +1,14 @@
+const Database = require('better-sqlite3');
+const db = new Database('scores.db');
+
+// Create table if not exists
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS scores (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
+        user_name TEXT,
+        score INTEGER
+    )
+`).run();
+
+module.exports = db;
