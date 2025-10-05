1214e49b1b4cd55fd1937a6f8d9302eb3b2e8c85
Miroslav Junker <junker@codeversa.com>
2025-10-06 01:02:56 +0200

    implement speed gauge

--- app-display/src/components/Gauges/Gauges.tsx
+++ app-display/src/components/Gauges/Gauges.tsx
@@ -15,8 +15,25 @@ export const Gauges = () => {
               width: 0.2,
               padding: 0.005,
               cornerRadius: 1,
-              colorArray: ['#ef4444', 'black'],
+              colorArray: ['#ef4444', '#111'],
               subArcs: [{limit: 10, showTick: false}, {}]
+            }}
+            pointer={{color: '#00FF00', type: 'arrow'}}
+          />
+        </div>
+
+        <div className='gauge gauge--speed'>
+          <div className='gauge__label'>SPEED</div>
+          <GaugeComponent
+            value={40}
+            labels={{valueLabel: {hide: true}, tickLabels: {hideMinMax: true}}}
+            type='semicircle'
+            arc={{
+              width: 0.2,
+              padding: 0.005,
+              cornerRadius: 1,
+              colorArray: ['#111', '#ef4444'],
+              subArcs: [{limit: 75, showTick: false}, {}]
             }}
             pointer={{color: '#00FF00', type: 'arrow'}}
           />
