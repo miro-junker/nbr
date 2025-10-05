8bd543ea7c87ec49ee78cc4585cf8c8828d83265
Miroslav Junker <junker@codeversa.com>
2025-10-05 19:27:43 +0200

    login screen tuning

--- app-display/src/components/ScreenLogin/ScreenLogin.tsx
+++ app-display/src/components/ScreenLogin/ScreenLogin.tsx
@@ -1,4 +1,4 @@
-import { Sound } from '@/components/Sound'
+import { Sound, Logo } from '@/components'
 import music from '@/sounds/music-login.mp3'
 import img_qr from './qr.png'
 import './ScreenLogin.css'
@@ -9,6 +9,9 @@ export const ScreenLogin = () => {
         <div className='screen-login'>
             <img className='screen-login__qr' src={img_qr} alt='QR code' />
             <Sound src={music} autoplay />
+            <div className='screen-login__logo'>
+                <Logo />
+            </div>
         </div>
     );
 };
