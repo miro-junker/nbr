bc323ae87b2fca126ddb154e375e6f04b0c51011
Miroslav Junker <junker@codeversa.com>
2025-10-05 20:15:17 +0200

    rework intro screen

--- app-display/src/components/ScreenIntro.tsx
+++ app-display/src/components/ScreenIntro.tsx
@@ -1,3 +1,7 @@
+import { Logo } from '@/components'
+import './ScreenIntro.css'
+
+
 interface StartupScreenProps {
     onStart: () => void
 }
@@ -6,39 +10,22 @@ interface StartupScreenProps {
 export const ScreenIntro: React.FC<StartupScreenProps> = ({ onStart }) => {
     return (
         <div
+            className='screen-intro__container'
             onClick={onStart}
             onTouchStart={onStart} // ensures touch devices also trigger start
             style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                width: '100vw',
-                background: 'linear-gradient(to bottom, #1e3c72, #2a5298)',
-                color: '#fff',
-                textAlign: 'center',
-                fontFamily: 'Arial, sans-serif',
-                cursor: 'pointer', // shows user can click anywhere
+
             }}
         >
             <h1 style={{ fontSize: '3rem', marginBottom: '2rem' }}>
                 Update Conference Flight
             </h1>
-            <button
-                style={{
-                    padding: '1.5rem 3rem',
-                    fontSize: '1.5rem',
-                    fontWeight: 'bold',
-                    borderRadius: '12px',
-                    border: 'none',
-                    backgroundColor: '#ff5722',
-                    color: '#fff',
-                    boxShadow: '0 4px 10px rgba(0,0,0,0.3)',
-                }}
-            >
+            <button className='screen-intro__cta'>
                 Start the Game
             </button>
+            <div className='screen-intro__logo'>
+                <Logo />
+            </div>
         </div>
     )
 }
