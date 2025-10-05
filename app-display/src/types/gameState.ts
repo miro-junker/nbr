b5cd766b184d9ced6b9de149acd797254c909e99
Miroslav Junker <junker@codeversa.com>
2025-10-05 22:32:42 +0200

    store fuel and speed information

--- app-display/src/types/gameState.ts
+++ app-display/src/types/gameState.ts
@@ -1,9 +1,11 @@
 export type TPos = [number, number, number]
 
 export type TGameState = {
+    fuel: number,
+    speed: number,
     planePosX: number,
     planePosY: number,
     planeRotX: number,
     planeRotY: number,
-    parachutePos: TPos
+    parachutePos: TPos,
 }
