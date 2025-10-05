e2c07c3b94e33b74d876252ae8a8528df16b206d
Miroslav Junker <junker@codeversa.com>
2025-10-05 15:46:09 +0200

    add horn sfx

--- app-display/src/components/Sound.tsx
+++ app-display/src/components/Sound.tsx
@@ -1,34 +1,34 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react'
 
 
 interface BackgroundMusicProps {
-    src: string;
-    loop?: boolean;
-    autoplay?: boolean;
+    src: string
+    loop?: boolean
+    autoplay?: boolean
 }
 
 
 export const Sound = ({ src, loop = true, autoplay = false }: BackgroundMusicProps) => {
-    const refAudio = useRef<HTMLAudioElement | null>(null);
+    const refAudio = useRef<HTMLAudioElement | null>(null)
 
     useEffect(() => {
-        const audio = new Audio(src);
-        audio.loop = loop;
-        audio.autoplay = autoplay;
-        audio.volume = 1; // set volume to max
-        refAudio.current = audio;
+        const audio = new Audio(src)
+        audio.loop = loop
+        audio.autoplay = autoplay
+        audio.volume = 1 // set volume to max
+        refAudio.current = audio
 
         if (autoplay) {
             audio.play().catch((err) => {
-                console.warn('Audio autoplay failed:', err);
-            });
+                console.warn('Audio autoplay failed:', err)
+            })
         }
 
         return () => {
-            audio.pause();
-            refAudio.current = null;
+            audio.pause()
+            refAudio.current = null
         };
-    }, [src, loop, autoplay]);
+    }, [src, loop, autoplay])
 
     return null; // No visible UI
 };
