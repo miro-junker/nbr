faf2885e23ee4f8467cb873eaa81454abd99d564
Miroslav Junker <junker@codeversa.com>
2025-10-06 20:55:20 +0200

    simplify plane component interface

--- app-display/src/components/Plane.tsx
+++ app-display/src/components/Plane.tsx
@@ -3,26 +3,16 @@ import { forwardRef } from 'react'
 import * as THREE from 'three'
 
 
-interface Props {
-    posX: number
-    posY: number
-    turnX: number
-    turnY: number
-}
-
-
-export const Plane = forwardRef<THREE.Object3D, Props>((props: Props, ref) => {
+export const Plane = forwardRef<THREE.Object3D>((_, ref) => {
     const gltf = useGLTF("3d/plane.gltf")
-
-    const { posX, posY, turnX, turnY } = props
 
     return (
         <primitive
             ref={ref}
             object={gltf.scene}
             scale={1}
-            rotation={[turnY, 0, turnX]}
-            position={[-posX, posY, 0]}
+            rotation={[0, 0, 0]}
+            position={[0, 0, 0]}
         />
     );
 })
