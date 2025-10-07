53e109689f85d93fa8b74574aee71e7ef0d2e0d2
Miroslav Junker <junker@codeversa.com>
2025-10-07 03:18:20 +0200

    cleanup constants

--- app-display/src/config/render.ts
+++ app-display/src/config/render.ts
@@ -1,9 +1,11 @@
-export const PLANE_MODEL_TILT_Y_COEF = 0.3
-export const PLANE_MODEL_TILT_Y_OFFSET = 0.1
+export const PLANE_MODEL_TILT_Y_OFFSET = 0.1  // static plane front-rear tilt
+export const PLANE_MODEL_TILT_Y_COEF   = 0.3  // dynamic plane front-rear tilt
 
 export const SKY_ROTATION = 0.5 * Math.PI
 export const SKY_HDRI = '3d/hdri_1k.hdr'
 
-export const REFRESH_RATE_APPSTATE = 1 // seconds
+export const REFRESH_RATE_APPSTATE = 1  // [sec]
 
 export const COLOR_PARACHUTE = '#555555'
+
+export const DURATION_GAMEOVER_SCREEN = 15  // [sec]
