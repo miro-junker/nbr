723c617b983b507f836e1552df8a5f8c3dfffa5b
Miroslav Junker <junker@codeversa.com>
2025-10-07 12:32:14 +0200

    better background position on login screen

--- app-display/src/config/game.ts
+++ app-display/src/config/game.ts
@@ -1,5 +1,5 @@
 export const DURATION_FUEL = 120  // [sec]
 
 export const SPEED_MIN = 1
-export const SPEED_MAX = 10
-export const SPEED_INCREMENT_COEF = 0.35
+export const SPEED_MAX = 15
+export const SPEED_INCREMENT_COEF = 0.2
